5744e03ac0f766ff5d96026feb0f7fac6049dd23
Dartamid <shabalin.dmm@yandex.ru>
2021-03-17 13:34:42 +0500

    fix for flake8

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -13,10 +13,3 @@ urlpatterns = [
     path("api/", include("api.urls")),
     path("account/", include("accounts.urls")),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns = (
-        [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    )
